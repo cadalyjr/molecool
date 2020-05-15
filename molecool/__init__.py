8714c7e292316c76e69890293365926d46370d41
Clyde Daly <cdaly2@haverford.edu>
2020-05-15 15:26:10 -0400

    changed config to match workshop

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -4,11 +4,13 @@ A Python package for analyzing and visualizing pdb and xyz files. For MolSSI May
 """
 
 # Add imports here
-from .functions import * #canvas
+from .functions import canvas, zen #canvas
 # it's considered "bad practice" to import *, as opposed to specfic functions
-# this syntax leads to molecool.canvas
-#import molecool.functions
-# this syntax leads to molecool.functions.canvas
+from .molecule import build_bond_list
+from .visualize import draw_molecule, bond_histogram
+from .measure import calculate_angle, calculate_distance
+from .atom_data import atomic_weights, atom_colors
+from molecool.io import open_pdb, open_xyz, write_xyz
 
 # Handle versioneer
 from ._version import get_versions
--- molecool/functions.py
+++ molecool/functions.py
@@ -90,18 +90,6 @@ def draw_molecule(coordinates, symbols, draw_bonds=None, save_location=None, dpi
 
     return ax
 
-def calculate_angle(rA, rB, rC, degrees=False):
-    # Calculate the angle between three points. Answer is given in radians by default, but can be given in degrees
-    # by setting degrees=True
-    AB = rB - rA
-    BC = rB - rC
-    theta=np.arccos(np.dot(AB, BC)/(np.linalg.norm(AB)*np.linalg.norm(BC)))
-
-    if degrees:
-        return np.degrees(theta)
-    else:
-        return theta
-
 def bond_histogram(bond_list, save_location=None, dpi=300, graph_min=0, graph_max=2):
     # Draw a histogram of bond lengths based on a bond_list (output from build_bond_list function)
 
